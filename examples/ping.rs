18cee19af77419c8f89ff9f31ea32404aebca020
Borodinov Ilya <borodinov.ilya@gmail.com>
2024-01-06 16:04:12 +0300

    Add arbiters and singleton support

--- examples/ping.rs
+++ examples/ping.rs
@@ -1,3 +1,27 @@
+enum Printer {
+    ToString(String),
+    ToStdout,
+}
+
+impl acril::Service for Printer {
+    type Context = acril_rt::Context<Self>;
+    type Error = ();
+}
+impl acril::Handler<String> for Printer {
+    type Response = ();
+    async fn call(
+        &mut self,
+        request: String,
+        _cx: &mut Self::Context,
+    ) -> Result<Self::Response, Self::Error> {
+        match self {
+            Self::ToStdout => println!("{request}"),
+            Self::ToString(s) => s.push_str(&request),
+        };
+        Ok(())
+    }
+}
+
 struct Pinger {
     count: u32,
 }
@@ -12,23 +36,34 @@ impl acril::Service for Pinger {
 impl acril::Handler<Ping> for Pinger {
     type Response = Pong;
 
-    async fn call(&mut self, _ping: Ping, _cx: &mut Self::Context) -> Result<Pong, Self::Error> {
-        println!("ping #{}", self.count);
+    async fn call(&mut self, _ping: Ping, cx: &mut Self::Context) -> Result<Pong, Self::Error> {
+        cx.singleton::<Printer>().await.send(format!("ping #{}", self.count)).await?;
 
         self.count += 1;
 
         Ok(Pong(self.count))
     }
 }
-#[tokio::main(flavor = "current_thread")]
-async fn main() {
-    let runtime = acril_rt::Runtime::new();
-    let addr = runtime.spawn(Pinger { count: 0 }).await;
 
-    println!("Liftoff!");
+fn main() -> std::thread::Result<()> {
+    let arbiter = acril_rt::Arbiter::new();
+    let runtime = acril_rt::Runtime::new_in(&arbiter);
 
-    loop {
-        let pong = addr.send(Ping).await.unwrap();
-        println!("pong #{}", pong.0);
-    }
+    arbiter.spawn(async move {
+        let printer = runtime.spawn(Printer::ToStdout).await;
+        let addr = runtime
+            .spawn(Pinger {
+                count: 0,
+            })
+            .await;
+
+        printer.do_send("Liftoff!".to_string());
+
+        loop {
+            let pong = addr.send(Ping).await.unwrap();
+            printer.send(format!("pong #{}", pong.0)).await.unwrap();
+        }
+    });
+
+    arbiter.join()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,30 +4,36 @@
 
 use std::{
     any::{Any, TypeId},
+    cell::RefCell,
     collections::HashMap,
     marker::PhantomData,
     pin::Pin,
-    sync::Arc, future::IntoFuture,
+    sync::Arc,
+    thread,
 };
 
 use acril::{Future, Handler, Service};
 use tokio::{
     sync::{
         mpsc::{self, unbounded_channel, Sender, UnboundedReceiver, UnboundedSender},
-        oneshot,
+        oneshot, RwLock,
     },
-    task::JoinHandle,
+    task::LocalSet,
 };
 
-type Unknown = Box<dyn Any + Sync + Send + 'static>;
+type Unknown = Box<dyn Any + Send + Sync + 'static>;
 type AnyProc = Unknown;
 type AnyMsg = Box<dyn Any + Sync + Send + 'static>;
 
 type ProcessRunner = Arc<
-    dyn Fn(AnyProc, AddrErased, AnyMsg) -> Pin<Box<dyn Future<Output = AnyProc> + Send>>
+    dyn Fn(
+            AnyProc,
+            AddrErased,
+            AnyMsg,
+            UnboundedSender<RuntimeCommand>,
+        ) -> Pin<Box<dyn Future<Output = AnyProc> + Send>>
         + Send
-        + Sync
-        + 'static,
+        + Sync,
 >;
 
 type ExitSignalSender = Box<
@@ -42,17 +48,26 @@ pub struct Addr<S: Service> {
     phantom: PhantomData<S>,
 }
 
+impl<S: Service> Clone for Addr<S> {
+    fn clone(&self) -> Self {
+        Self {
+            erased: self.erased.clone(),
+            phantom: PhantomData,
+        }
+    }
+}
+
+#[derive(Clone)]
 struct AddrErased {
     id: u32,
     runner_tx: MessageSender,
 }
 
-impl<S: Service<Context = Context<S>> + Send + Sync + 'static> Addr<S> {
+impl<S: Service<Context = Context<S>>> Addr<S> {
     pub async fn send<M>(&self, msg: M) -> Result<S::Response, S::Error>
     where
-        S: Handler<M, call(): Send>,
+        S: Handler<M, call(): Send> + Send + Sync + 'static,
         M: 'static + Send + Sync,
-        S::Context: Send + Sync,
         S::Error: Send + Sync,
         S::Response: Send + Sync,
     {
@@ -60,25 +75,76 @@ impl<S: Service<Context = Context<S>> + Send + Sync + 'static> Addr<S> {
 
         self.erased
             .runner_tx
-            .send((self.erased.id, message_handler(Some(tx)), Box::new(msg)))
+            .send((
+                self.erased.id,
+                message_handler::<S, M>(Some(tx)),
+                Box::new(msg),
+            ))
             .unwrap();
 
         rx.recv().await.unwrap()
+    }
+
+    /// Just send a message, without waiting for a response.
+    /// Be aware that by using this function you allow desynchonization to happen, as this function
+    /// doesn't wait for the response.
+    /// If you don't want desyncs to happen, use [`send`].
+    pub fn do_send<M>(&self, msg: M)
+    where
+        S: Handler<M, call(): Send> + Send + Sync + 'static,
+        M: 'static + Send + Sync,
+        S::Error: Send + Sync,
+        S::Response: Send + Sync,
+    {
+        self.erased
+            .runner_tx
+            .send((self.erased.id, message_handler::<S, M>(None), Box::new(msg)))
+            .unwrap()
     }
 }
 
 pub struct Context<S: Service<Context = Self>> {
     addr: Addr<S>,
+    commands: UnboundedSender<RuntimeCommand>,
+}
+
+impl<S: Service<Context = Self>> Context<S> {
+    pub fn this(&self) -> Addr<S> {
+        self.addr.clone()
+    }
+
+    pub async fn try_singleton<T: Service<Context = Context<T>> + Any>(&self) -> Result<Addr<T>, SingletonError> {
+        let (addr_send, addr_recv) = oneshot::channel();
+
+        let _ = self.commands.send(RuntimeCommand::GetAddrOf {
+            ty: TypeId::of::<T>(),
+            addr: addr_send,
+        });
+
+        addr_recv.await.expect("runtime has been shut down").map(|erased| Addr {
+            erased,
+            phantom: PhantomData,
+        })
+    }
+
+    /// Retrieve an address to the singleton service `T`.
+    /// This panics 
+    pub async fn singleton<T: Service<Context = Context<T>> + Any>(&self) -> Addr<T> {
+        self.try_singleton().await.unwrap_or_else(|error| panic!(
+            "A singleton {T} was not available: {error:?}",
+            T = std::any::type_name::<T>()
+        ))
+    }
 }
 
 fn message_handler<S, M>(responder: Option<Sender<Result<S::Response, S::Error>>>) -> ProcessRunner
 where
-    S: 'static + Send + Sync + Handler<M, call(): Send, Context = Context<S>>,
+    S: 'static + Handler<M, call(): Send, Context = Context<S>> + Send + Sync,
     M: 'static + Send + Sync,
     S::Error: Send + Sync,
     S::Response: Send + Sync,
 {
-    Arc::new(move |actor, erased, msg| {
+    Arc::new(move |actor, erased, msg, commands| {
         let mut proc = actor.downcast::<S>().unwrap();
         let msg = msg.downcast::<M>().unwrap();
         let responder = responder.clone();
@@ -88,6 +154,7 @@ where
                 .call(
                     *msg,
                     &mut Context {
+                        commands,
                         addr: Addr {
                             erased,
                             phantom: PhantomData::<S>,
@@ -105,44 +172,137 @@ where
     })
 }
 
-async fn event_loop(
-    mut new_processes: UnboundedReceiver<(AnyProc, oneshot::Sender<AddrErased>)>,
-) {
-    let mut processes: HashMap<u32, AnyProc> = HashMap::new();
-    let mut count: u32 = 0;
-    let (message_sender, mut messages): (MessageSender, _) = unbounded_channel();
+thread_local!(
+    static HANDLE: RefCell<Option<ArbiterHandle>> = RefCell::new(None);
+);
 
-    loop {
-        tokio::select! { biased;
-            Some((new_proc, pid_sender)) = new_processes.recv() => {
-                let id = count;
-                processes.insert(id, new_proc);
-                count += 1;
-                let _ = pid_sender.send(AddrErased { id, runner_tx: message_sender.clone() });
-            }
-            Some((id, runner, msg)) = messages.recv() => {
-                let proc = processes.remove(&id).unwrap();
-                let proc = runner(proc, AddrErased { id, runner_tx: message_sender.clone() }, msg).await;
-                processes.insert(id, proc);
-            }
-            else => break
+pub struct Arbiter {
+    thread_handle: thread::JoinHandle<()>,
+    arb: ArbiterHandle,
+}
+
+enum ArbiterCommand {
+    Stop,
+    Execute(Pin<Box<dyn Future<Output = ()> + Send>>),
+}
+
+impl Arbiter {
+    async fn runner(mut rx: UnboundedReceiver<ArbiterCommand>) {
+        while let Some(ArbiterCommand::Execute(fu)) = rx.recv().await {
+            tokio::task::spawn_local(fu);
         }
+    }
+
+    pub fn current() -> ArbiterHandle {
+        Self::try_current().expect("no arbiter was available")
+    }
+
+    pub fn try_current() -> Option<ArbiterHandle> {
+        HANDLE.with_borrow(|x| x.as_ref().map(|a| ArbiterHandle { tx: a.tx.clone() }))
+    }
+
+    pub fn handle(&self) -> ArbiterHandle {
+        self.arb.clone()
+    }
+
+    pub fn new() -> Self {
+        Self::with_tokio_rt(|| {
+            tokio::runtime::Builder::new_current_thread()
+                .build()
+                .unwrap()
+        })
+    }
+
+    pub fn with_tokio_rt(factory: impl Fn() -> tokio::runtime::Runtime + Send + 'static) -> Self {
+        let (tx, rx) = unbounded_channel();
+        let (ready_tx, ready_rx) = std::sync::mpsc::channel::<()>();
+
+        Self {
+            thread_handle: thread::Builder::new()
+                .name("acril-rt-arbiter".to_string())
+                .spawn({
+                    let tx = tx.clone();
+                    move || {
+                        let tokio = factory();
+                        let local_set = LocalSet::new();
+                        let _guard = local_set.enter();
+
+                        HANDLE.set(Some(ArbiterHandle { tx }));
+                        ready_tx.send(()).unwrap();
+
+                        tokio.block_on(local_set.run_until(Self::runner(rx)));
+                    }
+                })
+                .unwrap(),
+            arb: ArbiterHandle {
+                tx: {
+                    ready_rx.recv().unwrap();
+                    tx
+                },
+            },
+        }
+    }
+
+    pub fn join(self) -> std::thread::Result<()> {
+        self.thread_handle.join()
     }
 }
 
+#[derive(Clone)]
+pub struct ArbiterHandle {
+    tx: UnboundedSender<ArbiterCommand>,
+}
+
+impl std::ops::Deref for Arbiter {
+    type Target = ArbiterHandle;
+    fn deref(&self) -> &Self::Target {
+        &self.arb
+    }
+}
+
+impl ArbiterHandle {
+    pub fn spawn(&self, future: impl Future<Output = ()> + Send + 'static) -> bool {
+        self.tx
+            .send(ArbiterCommand::Execute(Box::pin(future)))
+            .is_ok()
+    }
+
+    pub fn stop(&self) -> bool {
+        self.tx.send(ArbiterCommand::Stop).is_ok()
+    }
+}
+
+enum RuntimeCommand {
+    Process {
+        ty: TypeId,
+        proc: AnyProc,
+        addr: oneshot::Sender<AddrErased>,
+    },
+    GetAddrOf {
+        ty: TypeId,
+        addr: oneshot::Sender<Result<AddrErased, SingletonError>>,
+    },
+}
+
 pub struct Runtime {
-    new_processes: UnboundedSender<(AnyProc, oneshot::Sender<AddrErased>)>,
-    join: JoinHandle<()>,
+    command_sender: UnboundedSender<RuntimeCommand>,
 }
 
 impl Runtime {
     pub fn new() -> Self {
-        let (proc_sender, proc_recv) = unbounded_channel();
+        let (command_sender, command_recv) = unbounded_channel();
 
-        Self {
-            new_processes: proc_sender,
-            join: tokio::spawn(event_loop(proc_recv)),
-        }
+        tokio::spawn(Self::event_loop(command_recv, command_sender.clone()));
+
+        Self { command_sender }
+    }
+
+    pub fn new_in(arbiter: &ArbiterHandle) -> Self {
+        let (command_sender, command_recv) = unbounded_channel();
+
+        arbiter.spawn(Self::event_loop(command_recv, command_sender.clone()));
+
+        Self { command_sender }
     }
 
     pub async fn spawn<S: Service<Context = Context<S>> + Send + Sync + 'static>(
@@ -151,17 +311,76 @@ impl Runtime {
     ) -> Addr<S> {
         let (addr_send, addr_recv) = oneshot::channel();
 
-        self.new_processes.send((Box::new(service), addr_send)).unwrap();
+        self.command_sender
+            .send(RuntimeCommand::Process {
+                ty: service.type_id(),
+                proc: Box::new(service),
+                addr: addr_send,
+            })
+            .unwrap();
 
-        Addr { erased: addr_recv.await.unwrap(), phantom: PhantomData }
+        Addr {
+            erased: addr_recv.await.unwrap(),
+            phantom: PhantomData,
+        }
+    }
+
+    async fn event_loop(
+        mut commands: UnboundedReceiver<RuntimeCommand>,
+        commands_sender: UnboundedSender<RuntimeCommand>,
+    ) {
+        let processes: Arc<RwLock<HashMap<u32, (TypeId, AnyProc)>>> =
+            Arc::new(RwLock::new(HashMap::new()));
+        let mut count: u32 = 0;
+        let (message_sender, mut messages): (MessageSender, _) = unbounded_channel();
+
+        loop {
+            tokio::select! {
+                Some((id, runner, msg)) = messages.recv() => {
+                            let (ty, proc) = processes.write().await.remove(&id).unwrap();
+
+                            tokio::task::spawn_local({ let processes = processes.clone(); let commands_sender = commands_sender.clone(); let runner_tx = message_sender.clone(); async move {
+                                let proc = runner(proc, AddrErased { id, runner_tx }, msg, commands_sender).await;
+                                processes.write().await.insert(id, (ty, proc));
+                            }});
+                }
+                Some(command) = commands.recv() => {
+                    match command {
+                        RuntimeCommand::Process { ty, proc, addr } => {
+                            let id = count;
+                            processes.write().await.insert(id, (ty, proc));
+                            count += 1;
+                            let _ = addr.send(AddrErased { id, runner_tx: message_sender.clone() });
+                        }
+                        RuntimeCommand::GetAddrOf { ty, addr } => {
+                            let _ = addr.send(
+                                only_one(processes.read().await.iter()
+                                    .filter(|(_id, (ty_, _))| *ty_ == ty))
+                                    .map(|(id, _)| AddrErased { id: *id, runner_tx: message_sender.clone() }).map_err(|e| if e.is_some() { SingletonError::MoreThanOne } else { SingletonError::NotPresent})
+                            );
+                        }
+                    }
+                }
+                else => break
+            }
+        }
     }
 }
 
-impl IntoFuture for Runtime {
-    type Output = <JoinHandle<()> as Future>::Output;
-    type IntoFuture = JoinHandle<()>;
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum SingletonError {
+    MoreThanOne,
+    NotPresent,
+}
 
-    fn into_future(self) -> Self::IntoFuture {
-        self.join
+/// Require the iterator to have only one item, erroring if it has less than one or more than one.
+/// If there were more than one items, the next item is present as Err(Some(next_item))
+pub fn only_one<I: Iterator>(mut iter: I) -> Result<I::Item, Option<I::Item>> {
+    let item = iter.next().ok_or(None)?;
+
+    if let Some(next) = iter.next() {
+        Err(Some(next))
+    } else {
+        Ok(item)
     }
 }
