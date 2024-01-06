28dfde3e7de51fd282b8cad32584fb7a91b4e061
Borodinov Ilya <borodinov.ilya@gmail.com>
2024-01-06 18:15:17 +0300

    Document the entire crate

--- examples/ping.rs
+++ examples/ping.rs
@@ -1,3 +1,12 @@
+//! In this example I'm showing off a few different things.
+//! - Singletons, with [`acril_rt::Context::singleton`];
+//! - Non-void responses, with [`GetOutput`]
+//! - "Dependency injection", if you can call it that, based on `#[cfg(debug_assertions)]` with
+//! [`Printer`]
+
+// we're toggling between stdout and string based on debug_assertions, so one of them is dead code
+// in any case
+#[allow(dead_code)]
 enum Printer {
     ToString(String),
     ToStdout,
@@ -7,6 +16,9 @@ impl acril::Service for Printer {
     type Context = acril_rt::Context<Self>;
     type Error = ();
 }
+
+struct GetOutput;
+
 impl acril::Handler<String> for Printer {
     type Response = ();
     async fn call(
@@ -19,6 +31,21 @@ impl acril::Handler<String> for Printer {
             Self::ToString(s) => s.push_str(&request),
         };
         Ok(())
+    }
+}
+
+impl acril::Handler<GetOutput> for Printer {
+    type Response = Option<String>;
+    async fn call(
+        &mut self,
+        _get_string: GetOutput,
+        _cx: &mut Self::Context,
+    ) -> Result<Self::Response, Self::Error> {
+        if let Self::ToString(ref s) = self {
+            Ok(Some(s.to_owned()))
+        } else {
+            Ok(None)
+        }
     }
 }
 
@@ -37,33 +64,58 @@ impl acril::Handler<Ping> for Pinger {
     type Response = Pong;
 
     async fn call(&mut self, _ping: Ping, cx: &mut Self::Context) -> Result<Pong, Self::Error> {
-        cx.singleton::<Printer>().await.send(format!("ping #{}", self.count)).await?;
+        cx.singleton::<Printer>()
+            .await
+            .send(format!("ping #{}", self.count))
+            .await?;
 
         self.count += 1;
 
         Ok(Pong(self.count))
     }
 }
+impl acril::Handler<GetOutput> for Pinger {
+    type Response = u32;
+    async fn call(
+        &mut self,
+        _request: GetOutput,
+        _cx: &mut Self::Context,
+    ) -> Result<Self::Response, Self::Error> {
+        Ok(self.count)
+    }
+}
 
-fn main() -> std::thread::Result<()> {
-    let arbiter = acril_rt::Arbiter::new();
-    let runtime = acril_rt::Runtime::new_in(&arbiter);
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    // make sure the runtime can spawn !Send tasks
+    let local_set = tokio::task::LocalSet::new();
+    let _guard = local_set.enter();
 
-    arbiter.spawn(async move {
+    let runtime = acril_rt::Runtime::new();
+
+    local_set.run_until(async move {
+        #[cfg(debug_assertions)]
         let printer = runtime.spawn(Printer::ToStdout).await;
-        let addr = runtime
-            .spawn(Pinger {
-                count: 0,
-            })
-            .await;
+        #[cfg(not(debug_assertions))]
+        let printer = runtime.spawn(Printer::ToString(String::new())).await;
+        let pinger = runtime.spawn(Pinger { count: 0 }).await;
 
-        printer.do_send("Liftoff!".to_string());
+        printer.send("Liftoff!".to_string()).await.unwrap();
 
-        loop {
-            let pong = addr.send(Ping).await.unwrap();
+        for i in 0..10000 {
+            let pong = pinger.send(Ping).await.unwrap();
+
+            // i is 0-based and pong is 1-based because the pinger increments before returning
+            assert_eq!(pong.0, i + 1);
+
             printer.send(format!("pong #{}", pong.0)).await.unwrap();
         }
-    });
 
-    arbiter.join()
+        assert_eq!(pinger.send(GetOutput).await.unwrap(), 10000);
+        #[cfg(not(debug_assertions))]
+        // assert that the output is Ok(Some(non_empty_string))
+        assert!(!printer.send(GetOutput).await.unwrap().unwrap().is_empty());
+
+        printer.send("We're done!".to_string()).await.unwrap();
+    }).await;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![doc = include_str!("../README.md")]
 #![forbid(unsafe_code)]
 #![allow(incomplete_features)]
 #![feature(return_type_notation)]
@@ -18,31 +19,32 @@ use tokio::{
         mpsc::{self, unbounded_channel, Sender, UnboundedReceiver, UnboundedSender},
         oneshot, RwLock,
     },
-    task::LocalSet,
+    task::{JoinHandle, LocalSet},
 };
 
+/// A convenience alias to a [`Box`]`<dyn `[`Any`]` + Send + Sync + 'static>`.
 type Unknown = Box<dyn Any + Send + Sync + 'static>;
+/// A type-erased service.
 type AnyProc = Unknown;
-type AnyMsg = Box<dyn Any + Sync + Send + 'static>;
+/// A type-erased message.
+type AnyMsg = Unknown;
 
+/// A type-erased function, which handles a message sent to a service.
 type ProcessRunner = Arc<
     dyn Fn(
             AnyProc,
             AddrErased,
             AnyMsg,
             UnboundedSender<RuntimeCommand>,
-        ) -> Pin<Box<dyn Future<Output = AnyProc> + Send>>
+        ) -> Pin<Box<dyn Future<Output = AnyProc>>>
         + Send
         + Sync,
 >;
 
-type ExitSignalSender = Box<
-    dyn FnOnce(Unknown) -> Pin<Box<dyn Future<Output = Unknown> + Send>> + Send + Sync + 'static,
->;
-
+/// The sender half of a channel for sending messages to running services.
 type MessageSender = UnboundedSender<(u32, ProcessRunner, AnyMsg)>;
-type Subscribers = HashMap<TypeId, Vec<(ProcessRunner, MessageSender)>>;
 
+/// A handle to a running [`Service`], allowing to send messages to it.
 pub struct Addr<S: Service> {
     erased: AddrErased,
     phantom: PhantomData<S>,
@@ -57,6 +59,7 @@ impl<S: Service> Clone for Addr<S> {
     }
 }
 
+/// An erased service address, containing an ID and message sender.
 #[derive(Clone)]
 struct AddrErased {
     id: u32,
@@ -64,9 +67,10 @@ struct AddrErased {
 }
 
 impl<S: Service<Context = Context<S>>> Addr<S> {
+    /// Send a message to the service pointed to by this address.
     pub async fn send<M>(&self, msg: M) -> Result<S::Response, S::Error>
     where
-        S: Handler<M, call(): Send> + Send + Sync + 'static,
+        S: Handler<M> + Send + Sync + 'static,
         M: 'static + Send + Sync,
         S::Error: Send + Sync,
         S::Response: Send + Sync,
@@ -86,12 +90,14 @@ impl<S: Service<Context = Context<S>>> Addr<S> {
     }
 
     /// Just send a message, without waiting for a response.
+    ///
     /// Be aware that by using this function you allow desynchonization to happen, as this function
     /// doesn't wait for the response.
-    /// If you don't want desyncs to happen, use [`send`].
+    ///
+    /// If you don't want desyncs to happen, use [`send`](Self::send).
     pub fn do_send<M>(&self, msg: M)
     where
-        S: Handler<M, call(): Send> + Send + Sync + 'static,
+        S: Handler<M> + Send + Sync + 'static,
         M: 'static + Send + Sync,
         S::Error: Send + Sync,
         S::Response: Send + Sync,
@@ -103,17 +109,35 @@ impl<S: Service<Context = Context<S>>> Addr<S> {
     }
 }
 
+/// A service context.
+///
+/// Any service that is spawned onto a [`Runtime`] needs to have this type as context.
 pub struct Context<S: Service<Context = Self>> {
     addr: Addr<S>,
     commands: UnboundedSender<RuntimeCommand>,
 }
 
 impl<S: Service<Context = Self>> Context<S> {
+    /// Get the address of this service.
+    ///
+    /// Beware that if you use this to send a message to yourself (with [`Addr::send`]), you will
+    /// encounter a deadlock because to handle a message you need send a message, which just makes
+    /// the message handler never finish.
     pub fn this(&self) -> Addr<S> {
         self.addr.clone()
     }
 
-    pub async fn try_singleton<T: Service<Context = Context<T>> + Any>(&self) -> Result<Addr<T>, SingletonError> {
+    /// Spawn a future onto the current arbiter.
+    pub fn spawn<F: Future + 'static>(&self, fut: F) -> JoinHandle<F::Output> {
+        tokio::task::spawn_local(fut)
+    }
+
+    /// Try to retrieve the address of a singleton service `T`.
+    /// If there are more than one or no instances of `T` running,
+    /// this method will return a [`SingletonError`].
+    pub async fn try_singleton<T: Service<Context = Context<T>> + Any>(
+        &self,
+    ) -> Result<Addr<T>, SingletonError> {
         let (addr_send, addr_recv) = oneshot::channel();
 
         let _ = self.commands.send(RuntimeCommand::GetAddrOf {
@@ -121,25 +145,34 @@ impl<S: Service<Context = Self>> Context<S> {
             addr: addr_send,
         });
 
-        addr_recv.await.expect("runtime has been shut down").map(|erased| Addr {
-            erased,
-            phantom: PhantomData,
-        })
+        addr_recv
+            .await
+            .expect("runtime has been shut down")
+            .map(|erased| Addr {
+                erased,
+                phantom: PhantomData,
+            })
     }
 
     /// Retrieve an address to the singleton service `T`.
-    /// This panics 
+    /// This method panics if the service wasn't running
+    /// or there were more than one instance of it.
     pub async fn singleton<T: Service<Context = Context<T>> + Any>(&self) -> Addr<T> {
-        self.try_singleton().await.unwrap_or_else(|error| panic!(
-            "A singleton {T} was not available: {error:?}",
-            T = std::any::type_name::<T>()
-        ))
+        self.try_singleton().await.unwrap_or_else(|error| {
+            panic!(
+                "A singleton {T} was not available: {error:?}",
+                T = std::any::type_name::<T>()
+            )
+        })
     }
 }
 
+/// Make a new message handler. This takes a responder, which is an optional channel for sending
+/// the response of the service. A context is created inline to avoid storing another thing inside
+/// of the event loop.
 fn message_handler<S, M>(responder: Option<Sender<Result<S::Response, S::Error>>>) -> ProcessRunner
 where
-    S: 'static + Handler<M, call(): Send, Context = Context<S>> + Send + Sync,
+    S: 'static + Handler<M, Context = Context<S>> + Send + Sync,
     M: 'static + Send + Sync,
     S::Error: Send + Sync,
     S::Response: Send + Sync,
@@ -172,37 +205,51 @@ where
     })
 }
 
-thread_local!(
+thread_local! {
+    /// A handle to the arbiter that the current task is running on.
     static HANDLE: RefCell<Option<ArbiterHandle>> = RefCell::new(None);
-);
+}
 
+/// An arbiter is a single-threaded event loop, allowing users to spawn tasks onto it.
 pub struct Arbiter {
     thread_handle: thread::JoinHandle<()>,
+    // store a handle instead of the tx itself so we can make Arbiter deref to the handle
+    // to not copy-paste spawn and stop methods
     arb: ArbiterHandle,
 }
 
+/// A command to an arbiter.
 enum ArbiterCommand {
+    /// Stop the arbiter
     Stop,
+    /// Execute a future.
     Execute(Pin<Box<dyn Future<Output = ()> + Send>>),
 }
 
 impl Arbiter {
     async fn runner(mut rx: UnboundedReceiver<ArbiterCommand>) {
+        // clever trick: the loop ends if there is a `None` or `Some(Stop)`
         while let Some(ArbiterCommand::Execute(fu)) = rx.recv().await {
             tokio::task::spawn_local(fu);
         }
     }
 
+    /// Get a handle to the arbiter that the current task is running in.
+    /// If the arbiter is not available, this function panics. If you don't want a panic, use
+    /// [`Self::try_current`], which returns an [`Option`]`<`[`ArbiterHandle`]`>`.
     pub fn current() -> ArbiterHandle {
         Self::try_current().expect("no arbiter was available")
     }
 
+    /// Get a handle to the arbiter that the current task is running in.
+    /// If the arbiter is not available, this returns [`None`].
     pub fn try_current() -> Option<ArbiterHandle> {
         HANDLE.with_borrow(|x| x.as_ref().map(|a| ArbiterHandle { tx: a.tx.clone() }))
     }
 
-    pub fn handle(&self) -> ArbiterHandle {
-        self.arb.clone()
+    /// Get a handle to this arbiter.
+    pub fn handle(&self) -> &ArbiterHandle {
+        &self.arb
     }
 
     pub fn new() -> Self {
@@ -227,10 +274,15 @@ impl Arbiter {
                         let local_set = LocalSet::new();
                         let _guard = local_set.enter();
 
+                        // "register" the arbiter
                         HANDLE.set(Some(ArbiterHandle { tx }));
+
                         ready_tx.send(()).unwrap();
 
                         tokio.block_on(local_set.run_until(Self::runner(rx)));
+
+                        // de-"register" the arbiter
+                        HANDLE.set(None);
                     }
                 })
                 .unwrap(),
@@ -248,6 +300,7 @@ impl Arbiter {
     }
 }
 
+/// A handle to an arbiter, allowing to spawn futures onto the arbiter or stop it.
 #[derive(Clone)]
 pub struct ArbiterHandle {
     tx: UnboundedSender<ArbiterCommand>,
@@ -272,18 +325,23 @@ impl ArbiterHandle {
     }
 }
 
+/// A command sent to a runtime.
 enum RuntimeCommand {
+    /// Spawn a service.
     Process {
         ty: TypeId,
         proc: AnyProc,
         addr: oneshot::Sender<AddrErased>,
     },
+    /// Get the address of a singleton service with said [`TypeId`].
     GetAddrOf {
         ty: TypeId,
         addr: oneshot::Sender<Result<AddrErased, SingletonError>>,
     },
 }
 
+/// The main component of `acril_rt` - the runtime. It stores the running services
+/// and handles their lifecycle and messages sent to them.
 pub struct Runtime {
     command_sender: UnboundedSender<RuntimeCommand>,
 }
@@ -292,7 +350,7 @@ impl Runtime {
     pub fn new() -> Self {
         let (command_sender, command_recv) = unbounded_channel();
 
-        tokio::spawn(Self::event_loop(command_recv, command_sender.clone()));
+        let _ = tokio::task::spawn_local(Self::event_loop(command_recv, command_sender.clone()));
 
         Self { command_sender }
     }
@@ -367,9 +425,12 @@ impl Runtime {
     }
 }
 
+/// An error returned while retrieving a singleton service.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum SingletonError {
+    /// There were more than one instances of the requested service.
     MoreThanOne,
+    /// The requested singleton service was not present.
     NotPresent,
 }
 
@@ -383,4 +444,11 @@ pub fn only_one<I: Iterator>(mut iter: I) -> Result<I::Item, Option<I::Item>> {
     } else {
         Ok(item)
     }
+}
+
+/// `use acril_rt::prelude::*;` to import the commonly used types.
+pub mod prelude {
+    #[doc(no_inline)]
+    pub use acril::{self, Handler, Service};
+    pub use crate::{Addr, Arbiter, Context, Runtime};
 }
